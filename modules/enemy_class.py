f77cc9e966a3fa68fb3821f5348347714bddcb63
Brittany Gates <support@bcgates.com>
2023-02-20 20:20:10 -0500

    Started the logic for the attack methods. I removed the text_hud.py file and moved that code into the Player and Enemy classes.

--- modules/enemy_class.py
+++ modules/enemy_class.py
@@ -7,6 +7,15 @@ class Enemy:
         self.weapon_dmg = weapon_dmg
         self.armor_rating = armor_rating
 
+    def enemy_hud(self):
+        """
+        Shows the current enemy's vitals on the screen to the player. 
+        return: The current status of enemy's `hp`, `weapon`, `weapon_dmg`, and `armor_rating`.
+        """
+        print("-" * 50)
+        print(f"{self}'s HP: {self.hp}\nCurrent Weapon: {self.weapon} | Dmg: {self.weapon_dmg}\nArmor Rating: -{self.armor_rating} Dmg")
+        print("-" * 50)
+
     # The method to attack the player goes below
 
     def attack_player(self):
@@ -17,5 +26,8 @@ class Enemy:
     def player_damage_taken(self):
         pass
 
-
-
+    def take_damage(self, damage):
+        remaining_hp = self.hp - damage
+        if remaining_hp >= 0:
+            self.hp = remaining_hp
+            print(f"{self} took {damage} points of damage and have {self.hp} left")
--- modules/player_class.py
+++ modules/player_class.py
@@ -9,7 +9,29 @@ class Player:
         self.weapon_dmg = 5
         self.armor_rating = 3
 
+    def player_hud(self):
+        """
+        Shows Dante's current vitals on the screen to the player. 
+        return: The current status of Dante's `hp`, `weapon`, `weapon_dmg`, `armor`, `armor_rating`, and treasure in his `backpack`.
+        """
+        print("-" * 50)
+        print(f"Dante's HP: {self.hp}\nCurrent Weapon: {self.weapon} | Dmg: {self.weapon_dmg}\nCurrent Armor: {self.armor} | Rating: -{self.armor_rating} Dmg\nDante's Loot: {self.backpack}")
+        print("-" * 50)
+
     # The method to attack the enemy goes below
+    """
+    Fighting enemies works as so:
+    1) Once a player encounters an enemy the fight starts
+    2) Use a random number to determine if the enemy catches the player by surprise.
+    3) If the enemy doesn't catch the player by surprise then the player attacks first.
+    4) The attack goes back and forth between the player and the enemy until one of them dies.
+    5) The attack uses random numbers to determine if the attack is successful.
+    6) A successful attack displays with "attack hit" or something on screen and shows the damage done.
+    7) A missed attacks displays with "miss" or something on the screen.
+    8) Damage done reduces the HP of the player and the enemy
+    9) If the player dies show a game over screen.
+    10) If the enemy dies the player gets to continue the game.
+    """
 
     def attack_enemy(self):
         pass
