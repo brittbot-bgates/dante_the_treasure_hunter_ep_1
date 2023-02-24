5ce92dbf5ffe07ea99cfa1d761a39dbe1730bcd9
Brittany Gates <support@bcgates.com>
2023-02-23 21:14:03 -0500

    Added the logic to first_move_decision.py to give out random loot after the first for the first choice. I also created the script for scene after the bat fight.

--- modules/loot.py
+++ modules/loot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+from modules.sleep_print import sleep_print
 from modules.character_class import Dante
 
 basic_loot = [
@@ -17,6 +18,7 @@ premium_loot = [
     "Gold bar",
     "A pouch of precious gemstones",
     "A full jewelry box",
+    "Pristine Silver Certificates",
 ]
 
 dante = Dante()
@@ -32,7 +34,10 @@ def generate_basic_loot():
     if num >= 2:
         basic_found_loot = random.choice(basic_loot)
         print(f"{basic_found_loot}!")
+        sleep_print()
         dante.backpack.append(basic_found_loot)
+        print(f"\nCurrent treasure inventory:\n")
+        Dante.player_hud(dante)
     else:
         print("Nothing.")
 
@@ -47,6 +52,9 @@ def generate_premium_loot():
     if num >= 3:
         premium_found_loot = random.choice(premium_loot)
         print(f"{premium_found_loot}!")
+        sleep_print()
         dante.backpack.append(premium_found_loot)
+        print(f"\nCurrent treasure inventory:\n")
+        Dante.player_hud(dante)
     else:
         print("Nothing.")
